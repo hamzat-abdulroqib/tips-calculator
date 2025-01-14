edc4508d672b2fe71b8f4a34ea4223c1dc728223
hamzat-abdulroqib <binhamzat0@gmail.com>
2025-01-14 15:44:57 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        verydarc: "	#00494d",
+        darkgrayish: "#5e7a7d",
+        grayishcyan: "	#7f9c9f",
+        lightgrayishcyan: "	#c5e4e7",
+        verylightgrayishcyan: "	#f4fafa",
+        white: "	#ffffff",
+        strongcyan: "	#26c0ab",
+      },
+    },
+  },
+  plugins: [],
+};
